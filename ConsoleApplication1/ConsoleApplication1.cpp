14451eca4716b1256112cc6a9a34d183ba16439c
rokezo <dobusnikita@gmail.com>
2024-06-04 21:17:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct Car {
+    double length;
+    double clearance;
+    double engineVolume;
+    double enginePower;
+    double wheelDiameter;
+    string color;
+    string transmissionType;
+};
+
+void setCarValues(Car& car) {
+    cout << "Введіть довжину автомобіля: ";
+    cin >> car.length;
+
+    cout << "Введіть кліренс (висоту посадки) автомобіля: ";
+    cin >> car.clearance;
+
+    cout << "Введіть об'єм двигуна автомобіля: ";
+    cin >> car.engineVolume;
+
+    cout << "Введіть потужність двигуна автомобіля: ";
+    cin >> car.enginePower;
+
+    cout << "Введіть діаметр коліс автомобіля: ";
+    cin >> car.wheelDiameter;
+
+    cout << "Введіть колір автомобіля: ";
+    cin >> car.color;
+
+    cout << "Введіть тип коробки передач автомобіля: ";
+    cin >> car.transmissionType;
+}
+
+void printCarValues(Car car) {
+    cout << "Довжина автомобіля: " << car.length << endl;
+    cout << "Кліренс (висота посадки) автомобіля: " << car.clearance << endl;
+    cout << "Об'єм двигуна автомобіля: " << car.engineVolume << endl;
+    cout << "Потужність двигуна автомобіля: " << car.enginePower << endl;
+    cout << "Діаметр коліс автомобіля: " << car.wheelDiameter << endl;
+    cout << "Колір автомобіля: " << car.color << endl;
+    cout << "Тип коробки передач автомобіля: " << car.transmissionType << endl;
+}
+
+void searchCarValues(Car car, string value) {
+    if (car.color == value) {
+        cout << "Знайдено автомобіль з кольором " << value << endl;
+    }
+    else {
+        cout << "Автомобіль з кольором " << value << " не знайдено" << endl;
+    }
+}
+
+int main() {
+    Car car;
+
+    setCarValues(car);
+    printCarValues(car);
+
+    string searchValue;
+    cout << "Введіть колір для пошуку: ";
+    cin >> searchValue;
+
+    searchCarValues(car, searchValue);
+
+    return 0;
+}
---
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Complex {
+    double real;
+    double imag;
+};
+
+Complex add(Complex a, Complex b) {
+    Complex result;
+    result.real = a.real + b.real;
+    result.imag = a.imag + b.imag;
+    return result;
+}
+
+Complex sub(Complex a, Complex b) {
+    Complex result;
+    result.real = a.real - b.real;
+    result.imag = a.imag - b.imag;
+    return result;
+}
+
+Complex mul(Complex a, Complex b) {
+    Complex result;
+    result.real = a.real * b.real - a.imag * b.imag;
+    result.imag = a.real * b.imag + a.imag * b.real;
+    return result;
+}
+
+Complex div(Complex a, Complex b) {
+    Complex result;
+    double denominator = b.real * b.real + b.imag * b.imag;
+    result.real = (a.real * b.real + a.imag * b.imag) / denominator;
+    result.imag = (a.imag * b.real - a.real * b.imag) / denominator;
+    return result;
+}
+
+void printComplex(Complex c) {
+    cout << c.real << " + " << c.imag << "i" << endl;
+}
+
+int main() {
+    Complex a, b, result;
+
+    cout << "Введіть перший комплексний номер: ";
+    cin >> a.real >> a.imag;
+
+    cout << "Введіть другий комплексний номер: ";
+    cin >> b.real >> b.imag;
+
+    result = add(a, b);
+    cout << "Сума: ";
+    printComplex(result);
+
+    result = sub(a, b);
+    cout << "Різниця: ";
+    printComplex(result);
+
+    result = mul(a, b);
+    cout << "Множення: ";
+    printComplex(result);
+
+    result = div(a, b);
+    cout << "Ділення: ";
+    printComplex(result);
+
+    return 0;
+}
